09e5ab0e1f384e2a1518e5ffad95899444ccb4af
tranthanhhieuthao <tranthanhhieuthao@gmail.com>
2020-06-29 23:04:42 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<button @click="saveAsTest"> create</button>
   </div>
 </template>
 
 <script>
+import { saveAs } from 'file-saver'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+    wbout: null
+    }
+  },
+  mounted() {
+  this.export()
+  },
+  methods: {
+    export() {
+      var XLSX = require('xlsx')
+      var wb = XLSX.utils.book_new()
+      // var wb1 = xl
+      wb.props = {
+         Title: "SheetJS Tutorial",
+         Subject: "Test",
+         Author: "Red Stapler",
+         CreatedDate: new Date(2020,7,1)
+      }
+      wb.SheetNames.push('Test Sheet')
+      var ws_data = [['hello', 'world']]
+      var ws = XLSX.utils.aoa_to_sheet(ws_data)
+      wb.Sheets['Test Sheet'] = ws
+      this.wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
+    },
+    s2ab(s) {
+                var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+                var view = new Uint8Array(buf);  //create uint8array as viewer
+                for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+                return buf;    
+     },
+     saveAsTest() {
+        saveAs(new Blob([this.s2ab(this.wbout)], {type:"application/octet-stream"}), 'test.xlsx')
+     }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import XLSX from 'xlsx'
 Vue.config.productionTip = false
-
+Vue.use(XLSX)
 new Vue({
+  XLSX,
   router,
   store,
   render: h => h(App)
